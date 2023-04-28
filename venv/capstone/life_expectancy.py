ef3e33585ebe9cc8e2add9c0232b6da1c2c45db8
rigphel <rigphel.dorjee@dcpl.bt>
2023-04-28 18:24:10 +1000

    prediction model using user input

--- venv/capstone/life_expectancy.py
+++ venv/capstone/life_expectancy.py
@@ -2,6 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 
 
 class LifeExpectancy:
@@ -10,12 +13,7 @@ class LifeExpectancy:
             page_title="Ex-stream-ly Cool App",
             page_icon="🧊",
             layout="wide",
-            initial_sidebar_state="expanded",
-            menu_items={
-                'Get Help': 'https://www.extremelycoolapp.com/help',
-                'Report a bug': "https://www.extremelycoolapp.com/bug",
-                'About': "# This is a header. This is an *extremely* cool app!"
-            }
+            initial_sidebar_state="expanded"
         )
 
         try:
@@ -23,16 +21,27 @@ class LifeExpectancy:
         except FileNotFoundError as e:
             st.error(f'File Not Found: {e}')
         else:
-            data_analysis_button, year_life_expectancy_button, region_life_button = st.columns(3)
+            self.main()
 
-            if data_analysis_button.button('Exploratory Data Analysis'):
-                self.exploratory_data_analysis()
+    def main(self):
+        st.header('Predict Life Expectancy')
 
-            if year_life_expectancy_button.button('Year Life Expectancy'):
-                self.year_life_expectancy()
+        lrm = self.train_model()
 
-            if region_life_button.button('Region Life Expectancy'):
-                self.region_life_expectancy()
+        self.predict_life_expectancy(lrm)
+
+        # Displaying buttons for user interaction
+        st.write('\n')
+        data_analysis_button, year_life_expectancy_button, region_life_button = st.columns(3)
+
+        if data_analysis_button.button('Exploratory Data Analysis'):
+            self.exploratory_data_analysis()
+
+        if year_life_expectancy_button.button('Year Life Expectancy'):
+            self.year_life_expectancy()
+
+        if region_life_button.button('Region Life Expectancy'):
+            self.region_life_expectancy()
 
     def exploratory_data_analysis(self):
         st.subheader('First 5 Rows')
@@ -67,6 +76,74 @@ class LifeExpectancy:
         sns.barplot(data=self.df, x='Region', y='Life_expectancy', hue='Region', ax=axis)
         sns.set(style='darkgrid', palette='Pastel1')
         st.pyplot(fig, use_container_width=False)
+
+    def train_model(self):
+        # Splitting data into features and target
+        X = self.df[['Infant_deaths', 'Adult_mortality', 'BMI', 'Population_mln', 'GDP_per_capita', 'Schooling',
+                     'Economy_status_Developed']]
+        y = self.df['Life_expectancy']
+
+        # Splitting data into train and test sets
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
+        # Fitting the model
+        lrm = LinearRegression()
+        lrm.fit(X_train, y_train)
+
+        # Making predictions on test set
+        predictions = lrm.predict(X_test)
+
+        # Evaluating model performance
+        score = metrics.explained_variance_score(y_test, predictions)
+
+        # Displaying predictions and model performance
+        st.write(f'Model Performance: {score * 100: .2f}%')
+
+        return lrm
+
+    def collect_user_inputs(self):
+        input_columns = ['Infant_deaths', 'Adult_mortality', 'BMI', 'Population_mln', 'GDP_per_capita', 'Schooling',
+                         'Economy_status_Developed']
+        user_input = []
+        for column in input_columns:
+            col = column.replace('_', ' ')
+            try:
+                user_input.append(st.text_input(f'Enter {col}'))
+            except ValueError:
+                st.error(f'Invalid input for {col}. Please enter a number.')
+                return None
+
+        # Creating input dataframe from user inputs
+        input_data = dict(zip(input_columns, user_input))
+        input_df = pd.DataFrame([input_data])
+
+        # Convert all columns to numeric data type
+        input_df = input_df.apply(pd.to_numeric, errors='coerce')
+
+        return input_df
+
+    def predict_life_expectancy(self, lrm):
+        # Collecting user inputs
+        input_df = self.collect_user_inputs()
+
+        if input_df is None:
+            # Error handling for invalid inputs
+            st.error('Invalid input. Please enter valid numerical values.')
+
+        else:
+            prediction_button = st.button('Predict')
+
+            if prediction_button:
+                try:
+                    # Making prediction on user input
+                    prediction = lrm.predict(input_df)
+
+                    # Displaying prediction on user input
+                    st.write('Prediction on User Input:', prediction)
+
+                except Exception as e:
+                    # Error handling for model prediction
+                    st.error('Prediction failed. Please check your inputs and try again.')
 
 
 if __name__ == '__main__':
